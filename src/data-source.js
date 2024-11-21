6238455ff9296c5fa20e7d3e22661b421060060a
Rahul Chaudhary <145273561+Rahulchaudharyji2@users.noreply.github.com>
2024-11-21 21:53:45 +0530

    Update data-source.js

--- src/data-source.js
+++ src/data-source.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 
 const dbUrl = process.env.MONGO_DB_URL || 'mongodb+srv://rahulchaudharyji2:s5lRXzU96ym1idoU@cluster0.nzxzs3o.mongodb.net/HospitalManagement?retryWrites=true&w=majority&appName=Cluster0';
 
-
-
-class Database{
+class Database {
     static async connect() {
         try {
-            await mongoose.connect(dbUrl);
-        
-        }
-        catch (err) {
+            await mongoose.connect(dbUrl, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000, // Set a timeout (5 seconds)
+            });
+            console.log('Database connected successfully');
+        } catch (err) {
+            console.error('Database connection error:', err.message);
+            // Additional logging for debugging
             console.error(err);
         }
     }
@@ -18,9 +21,9 @@ class Database{
     static async disconnect() {
         try {
             await mongoose.disconnect();
-        }
-        catch (err) {
-            console.error(err);
+            console.log('Database disconnected');
+        } catch (err) {
+            console.error('Error disconnecting from the database:', err.message);
         }
     }
 }
